f4a0a60f3e8d808f6df1e3f639576bfe332c75da
Shubham Agarwal <shubhamagarawal10@gmail.com>
2021-04-02 12:52:44 +0530

    React-boot-strap Setup for PROTOSO

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,20 @@
 import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+
 const App = () => {
-  return <h1> Welcome to PROTOSO </h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1> Welcome to PROTOSO</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
